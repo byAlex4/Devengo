8e095c2cdf59e2fbd29be93c75594c393e224e18
byAlex4 <alex.bocanegra.medina@gmail.com>
2023-11-10 13:18:19 -0600

    update vista de usuario

--- components/funciones/devengoDatos.php
+++ components/funciones/devengoDatos.php
@@ -3,28 +3,45 @@ header("Content-Type: application/json");
 try {
     // Conectamos con la base de datos
     include '../../config.php';
+    // Iniciar la sesión
+    session_start();
+    $unidad = $_SESSION['unidad'];
 
     // Preparamos la consulta SQL para obtener los datos de los usuarios, sus unidades y sus roles
     $stmt = $conexion->prepare(
-        "SELECT devengos.id, devengos.proveedros , devengos.fecha, devengos.descripcion, 
-        FORMAT(devengos.monto, 3) AS monto_formato, 
-        devengos.created_at, devengos.updated_at, devengos.contratoID AS contratoID,
-        contratos.clave AS contrato, 
-        FORMAT(contratos.mont_max, 3) AS saldo,
-        (contratos.mont_max - (SELECT SUM(monto) FROM devengos WHERE contratoID = contratos.id)) AS saldoDis, 
-        usuarios.nombre AS usuario, 
-        unidades.nombre AS unidad 
-        FROM devengos 
-        JOIN contratos ON devengos.contratoID = contratos.id 
-        JOIN usuarios ON devengos.usuarioID = usuarios.id 
-        JOIN unidades ON usuarios.unidadID = unidades.id"
+        "SELECT
+    devengos.id,
+    devengos.proveedros,
+    devengos.fecha,
+    devengos.descripcion,
+    FORMAT(devengos.monto, 3) AS monto_formato,
+    devengos.created_at,
+    devengos.updated_at,
+    devengos.contratoID AS contratoID,
+    contratos.clave AS contrato,
+    FORMAT(contratos.mont_max, 3) AS saldo, (
+        contratos.mont_max - (
+            SELECT SUM(monto)
+            FROM devengos
+            WHERE
+                contratoID = contratos.id
+        )
+    ) AS saldoDis,
+    usuarios.nombre AS usuario,
+    unidades.nombre AS unidad
+FROM devengos
+    JOIN contratos ON devengos.contratoID = contratos.id
+    JOIN usuarios ON devengos.usuarioID = usuarios.id
+    JOIN unidades ON usuarios.unidadID = unidades.id
+WHERE unidades.nombre = 'UMF2';"
     );
-
 
     // Ejecutamos la consulta SQL sin parámetros
     $stmt->execute();
     // Obtenemos el resultado como un array de arrays asociativos
     $resultados = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    //echo json_encode($stmt);
 
     // Comprobamos si hay resultados
     if (count($resultados) > 0) {
